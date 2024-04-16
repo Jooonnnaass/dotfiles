bd10216b4b03b655d6db87f6346ec3a89cdd63cd
jooonnnaass <produktiverjonas@gmail.com>
2024-04-16 18:00:01 +0200

    add treesitter, lsp, mini, crates, harpoon, noice

--- .config/nvim/lua/jooonnnaass/plugins/colorscheme.lua
+++ .config/nvim/lua/jooonnnaass/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
-return{
-	"catppuccin/nvim",
-	priority = 1000,
-	name = "cattppuccin",
-	config = function()
-		vim.cmd.colorscheme "catppuccin-frappe"
-	end,
+return {
+    "catppuccin/nvim",
+    priority = 1000,
+    name = "cattppuccin",
+    config = function()
+        vim.cmd.colorscheme "catppuccin-frappe"
+    end,
 }
