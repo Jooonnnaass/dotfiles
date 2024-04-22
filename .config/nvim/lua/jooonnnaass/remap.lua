dd7e9ec938786cf502d920d1b5223a44640fcf1e
jooonnnaass <produktiverjonas@gmail.com>
2024-04-22 11:46:56 +0200

    redo keymaps

--- .config/nvim/lua/jooonnnaass/remap.lua
+++ .config/nvim/lua/jooonnnaass/remap.lua
@@ -1,7 +1,21 @@
 vim.g.mapleader = " "
+-- open file explorer
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- move seleted line up or down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- format file
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- toggle terminal
+vim.keymap.set("n", "t", "<CMD>ToggleTerm<CR>")
+
+-- vsplit current buffer
+vim.keymap.set("n", "<leader>s", "<CMD>:vsplit<CR>")
+
+-- got to vsplit right
+vim.keymap.set("n", "<leader>l", "<C-w>l")
+-- got to vsplit left
+vim.keymap.set("n", "<leader>h", "<C-w>h")
