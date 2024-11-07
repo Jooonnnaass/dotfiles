8b58dcfcb854b95f5cd5eebabbfeca5ed5a10c95
Jooonnnaass <produktiverjonas@gmail.com>
2024-11-07 17:37:42 +0100

    adapt intall script to windows and add windows nvim config

--- install.sh
+++ install.sh
@@ -4,6 +4,15 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*|MINGW*|MINGW32*|MSYS*)  machine=Windows;;
+esac
+
+if [ "$machine" == "Mac" ]
+	then
 #==============
 # Update brew
 #==============
@@ -33,3 +42,26 @@ sudo rm -rf ~/.config/nvim > /dev/null 2>&1
 #==============
 
 ln -sf $SCRIPT_DIR/mac ~/.config
+
+elif [ "$machine" == "Windows" ]
+	then
+		
+#==============
+# Delete old configs
+#==============
+
+# Remove wezterm config
+sudo rm -rf ~/.config/wezterm > /dev/null 2>&1
+
+# Remove nvim config
+sudo rm -rf ~/AppData/Local/nvim > /dev/null 2>&1
+
+#==============
+# Create symlinks of source files 
+#==============
+
+ln -sf $SCRIPT_DIR/windows/wezterm ~/.config
+
+ln -sf $SCRIPT_DIR/windows/nvim ~/AppData/Local
+
+fi
