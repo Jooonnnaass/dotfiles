90a1bd009ebc13b37406920fe8058ab5621cfb0e
Jooonnnaass <produktiverjonas@gmail.com>
2024-11-09 20:53:10 +0100

    add toggleterm with remap

--- mac/nvim/lua/config/remap.lua
+++ mac/nvim/lua/config/remap.lua
@@ -5,6 +5,12 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<leader>s", ":vsplit<CR>")
 
+local toggleTerm = function()
+	vim.cmd(":ToggleTerm")
+end
+
+vim.keymap.set({ "n", "t" }, "<C-t>", toggleTerm)
+
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
