a0d7201af408b653b6fa8aaa38e6132e1515edfc
Jooonnnaass <produktiverjonas@gmail.com>
2024-11-09 20:38:06 +0100

    add autopairs indent lines noice and new remaps

--- mac/nvim/lua/config/remap.lua
+++ mac/nvim/lua/config/remap.lua
@@ -1,2 +1,11 @@
 vim.keymap.set("n", "<leader>pg", vim.cmd.Ex)
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>s", ":vsplit<CR>")
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
