185ea82a4b37ed6795ac6145b96abda739f0f8b0
Jooonnnaass <produktiverjonas@gmail.com>
2025-01-15 09:13:15 +0100

    change indentation settings

--- mac/nvim/lua/config/settings.lua
+++ mac/nvim/lua/config/settings.lua
@@ -1,11 +1,18 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
+vim.opt.expandtab = true
+
+--vim.api.nvim_create_autocmd("FileType", {
+--	attern = "html",
+--	callback = function()
+--		vim.opt_local.shiftwidth = 2
+--		vim.opt_local.tabstop = 2
+--	end
+--})
 
 vim.opt.wrap = false
 
